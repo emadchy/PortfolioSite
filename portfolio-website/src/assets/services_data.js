4509938d0b79bd5406f2fb07c93f9963f38a1879
emadchy96 <echowdhury7@myseneca.ca>
2024-09-08 00:11:15 -0400

    updated

--- portfolio-website/src/assets/services_data.js
+++ portfolio-website/src/assets/services_data.js
@@ -2,33 +2,34 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Creating visually appealing, responsive, and user-friendly websites tailored to meet client needs, ensuring a seamless user experience across all devices."
     },
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Crafting compelling and aesthetically pleasing visual content, including logos, branding materials, and digital media assets that resonate with your target audience."
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Developing and managing strategic social media campaigns that increase engagement, grow your online presence, and effectively connect with your audience."
     },
     {
         s_no:"04",
         s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Designing intuitive and user-centric mobile applications with a focus on functionality, aesthetics, and a seamless user experience across platforms."
     },
     {
         s_no:"05",
         s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Executing comprehensive digital marketing strategies, including SEO, SEM, email marketing, and paid advertising, to enhance brand visibility and drive conversions."
     },
     {
         s_no:"06",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Creating high-quality, engaging, and SEO-optimized content that communicates your brand's message effectively and drives traffic to your website."
     },
 ]
+
  
 export default Services_Data;
